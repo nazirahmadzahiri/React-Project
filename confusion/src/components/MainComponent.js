fb4005ab3057d1d45b04fbff5d4a32420d1c2ae7
Nazir Ahmad <nazirahmadzahiri93@gmail.com>
2020-12-25 01:09:50 +0300

    My First message

---
+++ confusion/src/components/MainComponent.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import Menu from './components/MenuComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import { DISHES } from './shared/dishes';
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      dishes:DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">My Resturant Items</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu  dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+}
+export default App;
